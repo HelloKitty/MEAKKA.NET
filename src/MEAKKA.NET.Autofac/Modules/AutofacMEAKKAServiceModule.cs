b4d873d4ba84babf4eb6ec12cb9801a2fcdf60ef
HelloKitty <andrew.blakely@ymail.com>
2021-01-06 12:48:57 -0600

    Added AutofacMEAKKAServiceModule

---
+++ src/MEAKKA.NET.Autofac/Modules/AutofacMEAKKAServiceModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.DI.AutoFac;
+using Akka.DI.Core;
+using Autofac;
+using Booma;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Registers default Autofac MEAKKA service modules.
+	/// </summary>
+	public sealed class AutofacMEAKKAServiceModule : Autofac.Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			//Prevent this from running multiple times.
+			if(builder.Properties.ContainsKey(typeof(AutofacMEAKKAServiceModule).AssemblyQualifiedName))
+				return;
+
+			builder.Properties.Add(typeof(AutofacMEAKKAServiceModule).AssemblyQualifiedName, null);
+
+			builder.RegisterType<AutoFacDependencyResolver>()
+				.As<IDependencyResolver>()
+				.InstancePerLifetimeScope();
+		}
+	}
+}
