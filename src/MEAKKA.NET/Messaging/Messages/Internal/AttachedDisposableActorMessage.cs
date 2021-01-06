7c509ae48517437a0cfa2d59adc552a1f27b7071
HelloKitty <andrew.blakely@ymail.com>
2021-01-06 11:21:42 -0600

    Created AttachedDisposableActorMessage

---
+++ src/MEAKKA.NET/Messaging/Messages/Internal/AttachedDisposableActorMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Internal MEAKKA message type that attaches a disposable to an Entity Actor.
+	/// </summary>
+	internal sealed class AttachedDisposableActorMessage : BaseInternalEntityActorMessage
+	{
+		/// <summary>
+		/// The disposable to attach.
+		/// </summary>
+		public IDisposable Disposable { get; }
+
+		public AttachedDisposableActorMessage(IDisposable disposable)
+		{
+			Disposable = disposable ?? throw new ArgumentNullException(nameof(disposable));
+		}
+	}
+}
