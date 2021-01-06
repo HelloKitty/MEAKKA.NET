5ad6493c3aa302b3f79f6e412862464c7d9e7a5c
HelloKitty <andrew.blakely@ymail.com>
2021-01-06 11:21:37 -0600

    Created BaseInternalEntityActorMessage

---
+++ src/MEAKKA.NET/Messaging/Messages/Internal/BaseInternalEntityActorMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Internal MEAKKA actor message base type.
+	/// </summary>
+	internal class BaseInternalEntityActorMessage
+	{
+
+	}
+}
