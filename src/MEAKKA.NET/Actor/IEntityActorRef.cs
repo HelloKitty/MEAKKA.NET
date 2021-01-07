6ebab6f37b85081ebb175fe1673585adb119901b
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 03:17:01 -0600

    Changed how actor refs are adapted to EntityActorRefs

--- src/MEAKKA.NET/Actor/IEntityActorRef.cs
+++ src/MEAKKA.NET/Actor/IEntityActorRef.cs
@@ -12,10 +12,15 @@ namespace MEAKKA
 	/// Entity Actor Type <typeparamref name="TActorType"/>.
 	/// </summary>
 	/// <typeparam name="TActorType"></typeparam>
-	public interface IEntityActorRef<TActorType> : IActorRef
+	public interface IEntityActorRef<TActorType>
 		where TActorType : IInternalActor
 	{
-
+		//The reason we no longer use an adapter style is AKKA.NET does some hacky type introspection
+		//which breaks if we adapt the actor ref.
+		/// <summary>
+		/// Adapted actor reference.
+		/// </summary>
+		IActorRef Actor { get; }
 	}
 
 	/// <summary>
@@ -23,60 +28,21 @@ namespace MEAKKA
 	/// Allowing for strongly typed actor references.
 	/// </summary>
 	/// <typeparam name="TActorType"></typeparam>
-	public sealed class EntityActorGenericAdapter<TActorType> : IEntityActorRef<TActorType>, IActorRef
+	public sealed class EntityActorGenericAdapter<TActorType> : IEntityActorRef<TActorType>
 		where TActorType : IInternalActor
 	{
 		/// <summary>
 		/// Adapted actor reference.
 		/// </summary>
-		private IActorRef Actor { get; }
+		public IActorRef Actor
+		{ 
+			[MethodImpl(MethodImplOptions.AggressiveInlining)] 
+			get;
+		}
 
 		public EntityActorGenericAdapter(IActorRef actor)
 		{
 			Actor = actor ?? throw new ArgumentNullException(nameof(actor));
-		}
-
-		/// <inheritdoc />
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public void Tell(object message, IActorRef sender)
-		{
-			Actor.Tell(message, sender);
-		}
-
-		/// <inheritdoc />
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public bool Equals(IActorRef other)
-		{
-			return Actor.Equals(other);
-		}
-
-		/// <inheritdoc />
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public int CompareTo(IActorRef other)
-		{
-			return Actor.CompareTo(other);
-		}
-
-		/// <inheritdoc />
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public ISurrogate ToSurrogate(ActorSystem system)
-		{
-			return Actor.ToSurrogate(system);
-		}
-
-		//TODO: Examine nullable
-		/// <inheritdoc />
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public int CompareTo(object obj)
-		{
-			return Actor.CompareTo(obj);
-		}
-
-		/// <inheritdoc />
-		public ActorPath Path
-		{
-			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			get => Actor.Path;
 		}
 	}
 }
