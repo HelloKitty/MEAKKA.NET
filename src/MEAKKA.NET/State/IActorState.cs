e90cd4d910b9028c7d9fd412256cacd759645da0
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 13:35:28 -0600

    Added IActorState interfaces

---
+++ src/MEAKKA.NET/State/IActorState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Simple mutable actor state interface.
+	/// </summary>
+	/// <typeparam name="T">State type.</typeparam>
+	public interface IMutableActorState<T>
+	{
+		/// <summary>
+		/// The actor state data.
+		/// </summary>
+		T Data { get; set; }
+	}
+
+	/// <summary>
+	/// Simple immutable actor state interface.
+	/// (Immutable through this interface but does not promise it won't mutate).
+	/// </summary>
+	/// <typeparam name="T">State type.</typeparam>
+	public interface IActorState<out T>
+	{
+		/// <summary>
+		/// The actor state data.
+		/// </summary>
+		T Data { get; }
+	}
+}
---
+++ src/MEAKKA.NET/State/MutableGenericActorState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Simple generic mutable <see cref="IActorState{T}"/> implementation.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public sealed class MutableGenericActorState<T> : IActorState<T>, IMutableActorState<T>
+	{
+		/// <inheritdoc />
+		public T Data { get; set; }
+
+		//To not confuse IoC we don't add a CTOR with this as a parameter.
+		/// <summary>
+		/// Empty uninitialized version of mutable state.
+		/// </summary>
+		public MutableGenericActorState()
+		{
+			
+		}
+	}
+}
