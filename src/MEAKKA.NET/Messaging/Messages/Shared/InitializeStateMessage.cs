09214e7ca5baaedf5686c45b4014f0f2d4c92951
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 01:25:40 -0600

    Added InitializeStateMessage to MEAKKA

---
+++ src/MEAKKA.NET/Messaging/Messages/Shared/InitializeStateMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Actor message ot initialize state.
+	/// </summary>
+	/// <typeparam name="T">The state type.</typeparam>
+	public sealed class InitializeStateMessage<T> : EntityActorMessage
+	{
+		/// <summary>
+		/// The state value to initialize.
+		/// </summary>
+		public T State { get; private set; }
+
+		public InitializeStateMessage(T state)
+		{
+			State = state ?? throw new ArgumentNullException(nameof(state));
+		}
+	}
+}
