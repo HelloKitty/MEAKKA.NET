67b7655aeae71ae28ecb0b49cc097b5f07e3dcdd
HelloKitty <andrew.blakely@ymail.com>
2021-01-09 07:52:24 -0600

    Added InitializeStateMessageHandler from Booma.Server

---
+++ src/MEAKKA.NET/Messaging/Handlers/InitializeStateMessageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MEAKKA;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Generic handler for <see cref="InitializeStateMessage{T}"/> that will assign the state
+	/// to the provided <see cref="IMutableActorState{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The state to handle initialization for.</typeparam>
+	public class InitializeStateMessageHandler<T> : BaseActorMessageHandler<InitializeStateMessage<T>>
+	{
+		/// <summary>
+		/// State container.
+		/// </summary>
+		protected IMutableActorState<T> MutableStateContainer { get; }
+
+		protected InitializeStateMessageHandler(IMutableActorState<T> mutableStateContainer)
+		{
+			MutableStateContainer = mutableStateContainer ?? throw new ArgumentNullException(nameof(mutableStateContainer));
+		}
+
+		public override Task HandleMessageAsync(EntityActorMessageContext context, InitializeStateMessage<T> message, CancellationToken token = default)
+		{
+			//Just initialize the state container.
+			MutableStateContainer.Data = message.State;
+			return Task.CompletedTask;
+		}
+	}
+}
