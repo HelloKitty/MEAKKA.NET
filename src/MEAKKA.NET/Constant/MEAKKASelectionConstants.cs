03d4dddad365f3a4988da42f9f6cdda1cbcee11a
HelloKitty <andrew.blakely@ymail.com>
2021-01-09 07:23:23 -0600

    Added MEAKKASelectionConstants

---
+++ src/MEAKKA.NET/Constant/MEAKKASelectionConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEAKKA
+{
+	public static class MEAKKASelectionConstants
+	{
+		/// <summary>
+		/// The selector string for all direct children for actor selection.
+		/// </summary>
+		public const string ALL_DIRECT_CHILDREN_SELECTOR = "*";
+	}
+}
