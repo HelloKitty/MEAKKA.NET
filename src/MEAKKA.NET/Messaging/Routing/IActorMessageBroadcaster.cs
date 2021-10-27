fe81cae8e897d4c4e93fb274de3e9b9377ab4a15
HelloKitty <andrew.blakely@ymail.com>
2021-10-26 23:07:00 -0500

    Moved DefaultGenericActorMessageBroadcaster

--- src/MEAKKA.NET/Messaging/Routing/IActorMessageBroadcaster.cs
+++ src/MEAKKA.NET/Messaging/Routing/IActorMessageBroadcaster.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using Akka.Actor;
-using Akka.Routing;
 using MEAKKA;
 
 namespace MEAKKA
@@ -42,77 +40,5 @@ namespace MEAKKA
 		/// <param name="group">The group type.</param>
 		/// <param name="actor">The actor to remove from the group.</param>
 		void RemoveFromGroup(TActorGroupType group, IActorRef actor);
-	}
-
-	/// <summary>
-	/// Default <see cref="IActorMessageBroadcaster{TActorGroupType}"/> implementation
-	/// that maintains a non-threadsafe broadcast group map to broadcast messages to.
-	/// </summary>
-	/// <typeparam name="TActorGroupType"></typeparam>
-	public sealed class DefaultGenericActorMessageBroadcaster<TActorGroupType> : IActorMessageBroadcaster<TActorGroupType> 
-		where TActorGroupType : Enum
-	{
-		private Dictionary<TActorGroupType, IActorRef> BroadcastRouteMap { get; } = new();
-
-		private IActorContext Parent { get; }
-
-		public DefaultGenericActorMessageBroadcaster(IActorContext parent)
-		{
-			Parent = parent ?? throw new ArgumentNullException(nameof(parent));
-		}
-
-		/// <inheritdoc />
-		public void BroadcastMessage(TActorGroupType group, EntityActorMessage message)
-		{
-			if (@group == null) throw new ArgumentNullException(nameof(@group));
-			if (message == null) throw new ArgumentNullException(nameof(message));
-
-			EnsureBroadcastGroupExists(group).Tell(new Broadcast(message));
-		}
-
-		/// <inheritdoc />
-		public void BroadcastMessage(TActorGroupType group, EntityActorMessage message, IActorRef sender)
-		{
-			if (@group == null) throw new ArgumentNullException(nameof(@group));
-			if (message == null) throw new ArgumentNullException(nameof(message));
-			if (sender == null) throw new ArgumentNullException(nameof(sender));
-
-			EnsureBroadcastGroupExists(group).Tell(new Broadcast(message), sender);
-		}
-
-		/// <inheritdoc />
-		public void AddToGroup(TActorGroupType group, IActorRef actor)
-		{
-			if (@group == null) throw new ArgumentNullException(nameof(@group));
-			if (actor == null) throw new ArgumentNullException(nameof(actor));
-
-			EnsureBroadcastGroupExists(group).Tell(new AddRoutee(Routee.FromActorRef(actor)));
-		}
-
-		/// <inheritdoc />
-		public void RemoveFromGroup(TActorGroupType group, IActorRef actor)
-		{
-			if (@group == null) throw new ArgumentNullException(nameof(@group));
-			if (actor == null) throw new ArgumentNullException(nameof(actor));
-
-			EnsureBroadcastGroupExists(group).Tell(new RemoveRoutee(Routee.FromActorRef(actor)));
-		}
-
-		/// <summary>
-		/// Ensures a broadcast actor exists for the specified group type.
-		/// If it doesn't exist it creates it and puts it in <see cref="BroadcastRouteMap"/>.
-		/// </summary>
-		/// <param name="group">Broadcast group type.</param>
-		/// <returns>The broadcast actor.</returns>
-		/// <exception cref="ArgumentNullException">Throws if group is null.</exception>
-		public IActorRef EnsureBroadcastGroupExists(TActorGroupType group)
-		{
-			if (@group == null) throw new ArgumentNullException(nameof(@group));
-
-			if (!BroadcastRouteMap.ContainsKey(group))
-				BroadcastRouteMap[group] = Parent.ActorOf(Props.Empty.WithRouter(new BroadcastGroup()));
-
-			return BroadcastRouteMap[group];
-		}
 	}
 }
