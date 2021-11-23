09b4701d689d8addb8f9d55e03213f4817c12f82
HelloKitty <andrew.blakely@ymail.com>
2021-11-22 22:09:57 -0600

    Added BroadcastTargeted internal message

---
+++ src/MEAKKA.NET/Messaging/Messages/Internal/BroadcastTargeted.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.Routing;
+
+namespace MEAKKA
+{
+	/// <summary>
+	/// Internal message used to broadcast a message to a specific actor target.
+	/// </summary>
+	internal sealed class BroadcastTargeted : Broadcast
+	{
+		//TODO: This won't be serializable
+		/// <summary>
+		/// The target of the message.
+		/// </summary>
+		public IActorRef Target { get; }
+
+		public BroadcastTargeted(EntityActorMessage message, IActorRef target) 
+			: base(message)
+		{
+			Target = target ?? throw new ArgumentNullException(nameof(target));
+		}
+	}
+}
